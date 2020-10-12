e0357d84232b9c3672ecc4b69700267c017079cc
soniacomp <syparkehsc@naver.com>
2020-10-13 01:27:16 +0900

    leet_641

--- python/sangil_week2/leet_641.py
+++ python/sangil_week2/leet_641.py
@@ -1,67 +1,68 @@
 class MyCircularDeque:
-
+    
     def __init__(self, k: int):
-        """
-        Initialize your data structure here. Set the size of the deque to be k.
-        """
-        
+        self.deque = []
+        self.max_count = k
 
     def insertFront(self, value: int) -> bool:
-        """
-        Adds an item at the front of Deque. Return true if the operation is successful.
-        """
-        
+        if self.isFull():
+            return False
+        self.deque.extend([self.getRear()])
+        for idx, _ in reversed(list(enumerate(self.deque))):
+            if idx == len(self.deque)-1:
+                continue
+            self.deque[idx+1] = self.deque[idx]
+        self.deque[0] = value
+        return True
 
     def insertLast(self, value: int) -> bool:
-        """
-        Adds an item at the rear of Deque. Return true if the operation is successful.
-        """
+        if self.isFull():
+            return False
+        self.deque.append(value)
+        return True
         
 
     def deleteFront(self) -> bool:
-        """
-        Deletes an item from the front of Deque. Return true if the operation is successful.
-        """
+        if self.isEmpty():
+            return False
+        for idx, _ in reversed(list(enumerate(self.deque))):
+            if idx == len(self.deque)-1:
+                continue
+            self.deque[idx] = self.deque[idx+1]
+        self.deque.pop()
+        return True
         
 
     def deleteLast(self) -> bool:
-        """
-        Deletes an item from the rear of Deque. Return true if the operation is successful.
-        """
+        if self.isEmpty():
+            return False
+        self.deque.pop()
+        return True
         
 
     def getFront(self) -> int:
-        """
-        Get the front item from the deque.
-        """
-        
+        if self.isEmpty():
+            return -1
+        return self.deque[0]
 
     def getRear(self) -> int:
-        """
-        Get the last item from the deque.
-        """
-        
+          if self.isEmpty():
+            return -1
+          return self.deque[len(self.deque)-1]
 
     def isEmpty(self) -> bool:
-        """
-        Checks whether the circular deque is empty or not.
-        """
+          if len(self.deque):
+            return False
+          return True
         
 
     def isFull(self) -> bool:
-        """
-        Checks whether the circular deque is full or not.
-        """
+          if len(self.deque) == self.max_count:
+            return True
+          return False
         
-
-
-# Your MyCircularDeque object will be instantiated and called as such:
-# obj = MyCircularDeque(k)
-# param_1 = obj.insertFront(value)
-# param_2 = obj.insertLast(value)
-# param_3 = obj.deleteFront()
-# param_4 = obj.deleteLast()
-# param_5 = obj.getFront()
-# param_6 = obj.getRear()
-# param_7 = obj.isEmpty()
-# param_8 = obj.isFull()
+  '''
+  오류 케이스
+["MyCircularDeque","insertFront","getRear","deleteLast","getRear","insertFront","insertFront","insertFront","insertFront","isFull","insertFront","isFull","getRear","deleteLast","getFront","getFront","insertLast","deleteFront","getFront","insertLast","getRear","insertLast","getRear","getFront","getFront","getFront","getRear","getRear","insertFront","getFront","getFront","getFront","getFront","deleteFront","insertFront","getFront","deleteLast","insertLast","insertLast","getRear","getRear","getRear","isEmpty","insertFront","deleteLast","getFront","deleteLast","getRear","getFront","isFull","isFull","deleteFront","getFront","deleteLast","getRear","insertFront","getFront","insertFront","insertFront","getRear","isFull","getFront","getFront","insertFront","insertLast","getRear","getRear","deleteLast","insertFront","getRear","insertLast","getFront","getFront","getFront","getRear","insertFront","isEmpty","getFront","getFront","insertFront","deleteFront","insertFront","deleteLast","getFront","getRear","getFront","insertFront","getFront","deleteFront","insertFront","isEmpty","getRear","getRear","getRear","getRear","deleteFront","getRear","isEmpty","deleteFront","insertFront","insertLast","deleteLast"]
+[[77],[89],[],[],[],[19],[23],[23],[82],[],[45],[],[],[],[],[],[74],[],[],[98],[],[99],[],[],[],[],[],[],[8],[],[],[],[],[],[75],[],[],[35],[59],[],[],[],[],[22],[],[],[],[],[],[],[],[],[],[],[],[21],[],[26],[63],[],[],[],[],[87],[76],[],[],[],[26],[],[67],[],[],[],[],[36],[],[],[],[72],[],[87],[],[],[],[],[85],[],[],[91],[],[],[],[],[],[],[],[],[],[34],[44],[]]
+  '''
