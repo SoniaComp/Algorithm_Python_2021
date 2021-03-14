92afd84b7bf7bc13ac4013e50b98455d16dfe2df
soniacomp <syparkehsc@naver.com>
2021-03-14 23:46:32 +0900

    어제 복습 및 코드 향상 - 파이썬스럽다

--- study/kakao2021/1.py
+++ study/kakao2021/1.py
@@ -1,33 +1,48 @@
 import re
 
-def trim_dot(string):
-    if string == '.' or string == '':
-        return ''
-    if string[0] == '.':
-        string = string[1:]
-    if string[-1] == '.':
-        string = string[:-1]
-    return string
+# def trim_dot(string):
+#     if string == '.' or string == '':
+#         return ''
+#     if string[0] == '.':
+#         string = string[1:]
+#     if string[-1] == '.':
+#         string = string[:-1]
+#     return string
 
 def solution(new_id):
-    answer = new_id
-    # 1
-    if not answer.islower():
-        answer = answer.lower()
-    # 2
-    answer = re.sub('[^a-z0-9-_\.]', '', answer)
-    # 3
-    answer = re.sub('\.{2,}', '.', answer)
-    # 4
-    answer = trim_dot(answer)
-    # 5,6
-    id_length = len(answer)
-    if id_length == 0:
-        answer = 'a'
-    elif id_length >= 16:
-        answer = answer[:15]
-        answer = trim_dot(answer)
-    # 7
-    if len(answer) <= 2:
-        answer += answer[-1] * (3 - len(answer))    
-    return answer
+#     answer = new_id
+    st = new_id
+#     # 1
+#     if not answer.islower():
+#         answer = answer.lower()
+    st = st.lower()
+#     # 2
+#     answer = re.sub('[^a-z0-9-_\.]', '', answer)
+    st = re.sub('[^a-z0-9\-_.]', '', st)
+#     # 3
+#     answer = re.sub('\.{2,}', '.', answer)
+    st = re.sub('\.+', '.', st)
+#     # 4
+#     answer = trim_dot(answer)
+    st = re.sub('^[.]|[.]$', '', st)
+#     # 5,6
+#     id_length = len(answer)
+#     if id_length == 0:
+#         answer = 'a'
+#     elif id_length >= 16:
+#         answer = answer[:15]
+#         answer = trim_dot(answer)
+    st = 'a' if len(st) == 0 else st[:15]
+    st = re.sub('^[.]|[.]$', '', st)
+#     # 7
+#     if len(answer) <= 2:
+#         answer += answer[-1] * (3 - len(answer))    
+    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
+#     return answer
+    return st
+
+# for 대신 inline for문 / range를 사용하고
+# if 대신 삼항 연산자를 사용하는 것이
+# 더 파이썬 스럽구나를 느꼈다.
+# 정규식 튜토리얼의 고수님을 만났당.. 
+# https://chrisjune-13837.medium.com/%EC%A0%95%EA%B7%9C%EC%8B%9D-%ED%8A%9C%ED%86%A0%EB%A6%AC%EC%96%BC-%EC%98%88%EC%A0%9C%EB%A5%BC-%ED%86%B5%ED%95%9C-cheatsheet-%EB%B2%88%EC%97%AD-61c3099cdca8
--- study/kakao2021/2.py
+++ study/kakao2021/2.py
@@ -1,29 +1,52 @@
 from itertools import combinations
+from collections import Counter
+
 
 def solution(orders, course):
-    answer = []
-    for course_num in course:
-        orders_list = list(filter(lambda x: len(x) >= course_num, orders))
-        courses_checked = {}
-        for idx in range(len(orders_list)):
-            order = orders_list[idx]
-            course_candidates = list(combinations(order, course_num))
-            for course_candidate in course_candidates:
-                candidate_key = ''.join(sorted(list(course_candidate)))
-                if not (candidate_key in courses_checked):
-                    count = 1
-                    for i in range(idx+1, len(orders_list)):
-                        if not (set(course_candidate) - set(orders_list[i])):
-                            count += 1
-                    if count >= 2:
-                        courses_checked[candidate_key] = count
-        sorted_courses_checked = sorted(courses_checked.items(), key=lambda x: x[1], reverse=True)
-        if len(sorted_courses_checked):
-            max = sorted_courses_checked[0][1]
-            for c in sorted_courses_checked:
-                if c[1] == max:
-                    answer.append(c[0])
-                else:
-                    break
-    answer.sort()
-    return answer
+    #     answer = []
+    result = []
+#     for course_num in course:
+    for course_size in course:
+        order_combinations = []
+#         orders_list = list(filter(lambda x: len(x) >= course_num, orders))
+#         courses_checked = {}
+#         for idx in range(len(orders_list)):
+#             order = orders_list[idx]
+#             course_candidates = list(combinations(order, course_num))
+#             for course_candidate in course_candidates:
+#                 candidate_key = ''.join(sorted(list(course_candidate)))
+#                 if not (candidate_key in courses_checked):
+#                     count = 1
+#                     for i in range(idx+1, len(orders_list)):
+#                         if not (set(course_candidate) - set(orders_list[i])):
+#                             count += 1
+#                     if count >= 2:
+#                         courses_checked[candidate_key] = count
+        for order in orders:
+            order_combinations += combinations(sorted(order), course_size)
+#         sorted_courses_checked = sorted(courses_checked.items(), key=lambda x: x[1], reverse=True)
+#         if len(sorted_courses_checked):
+#             max = sorted_courses_checked[0][1]
+#             for c in sorted_courses_checked:
+#                 if c[1] == max:
+#                     answer.append(c[0])
+#                 else:
+#                     break
+        most_ordered = Counter(order_combinations).most_common()
+        result += [k for k, v in most_ordered if v >
+                   1 and v == most_ordered[0][1]]
+#     answer.sort()
+#     return answer
+    return [''.join(v) for v in sorted(result)]
+    # 마지막에 처리해주기
+
+# class collections.Counter([iterable-or-mapping])¶
+# Counter는 해시 가능한 객체를 세기 위한 dict 서브 클래스입니다. 요소가 딕셔너리 키로 저장되고 개수가 딕셔너리값으로 저장되는 컬렉션입니다. 개수는 0이나 음수를 포함하는 임의의 정숫값이 될 수 있습니다. Counter 클래스는 다른 언어의 백(bag)이나 멀티 셋(multiset)과 유사합니다.
+
+# collections Counter
+# -> 0으로 초기화하지 않아도 된다는 장점을 가지고 있다.
+
+# https://www.daleseo.com/python-collections-counter/
+# https://wikidocs.net/84392 collections의 다른 함수
+# Counter 클래스는 파이썬의 기본 자료구조인 사전(dictionary)를 확장하고 있기 때문에, 사전에서 제공하는 API를 그대로 다 사용할 수가 있습니다.
+# 카운팅을 더 잘할 수 있도록, 데이터의 개수가 많은 순으로 정렬된 배열을 리턴하는 most_common이라는 메서드를 제공하고 있습니다.
